d7c8dd4cee1e1c4569dc31f74a60af7e84d3dca1
kraego <kraego@github.com>
2023-12-02 09:00:49 +0100

    Day 1 done

--- Day1.hs
+++ Day1.hs
@@ -2,22 +2,44 @@ import Data.Char (isDigit, digitToInt)
 import Data.Text(unpack)
 import qualified Data.Text    as Text
 import qualified Data.Text.IO as Text
+import Control.Monad 
 
+-- address overlapping
+replaceNumStrings :: [Char] -> [Char]
+replaceNumStrings xs 
+    | null xs = xs
+    | take 3 xs == "one"   = "o1e" ++ replaceNumStrings (drop 3 xs)
+    | take 3 xs == "two"   = "t2o" ++ replaceNumStrings (drop 3 xs)
+    | take 5 xs == "three" = "t3e" ++ replaceNumStrings (drop 3 xs)
+    | take 4 xs == "four"  = "f4r" ++ replaceNumStrings (drop 3 xs)
+    | take 4 xs == "five"  = "f5e" ++ replaceNumStrings (drop 3 xs)
+    | take 3 xs == "six"   = "s6x" ++ replaceNumStrings (drop 3 xs)
+    | take 5 xs == "seven" = "s7n" ++ replaceNumStrings (drop 3 xs)
+    | take 5 xs == "eight" = "e8t" ++ replaceNumStrings (drop 3 xs)
+    | take 4 xs == "nine"  = "n9e" ++ replaceNumStrings (drop 3 xs)
+    | otherwise = [head xs] ++ replaceNumStrings (tail xs)
+ 
 firstDigit :: [Char] -> Char
 firstDigit (x:xs)
     | isDigit x = x
     | otherwise = firstDigit xs
 
 sumLine :: [Char] -> Int
-sumLine xs = read([firstDigit xs] ++ [firstDigit (reverse xs)])
+sumLine xs = read $ [firstDigit xs] ++ [firstDigit (reverse xs)]
 
 sumWholeText :: [[Char]] -> Int
 sumWholeText [] = 0
 sumWholeText (x:xs) = (sumLine x) + sumWholeText xs
 
--- doIt "Day1aInput.txt"
-doIt :: FilePath -> IO (Int)
+doIt :: FilePath -> IO Int
 doIt fp = do
   contents <- readFile fp
-  let fileLines = lines contents
-  return (sumWholeText fileLines)
+  let fileLines = map replaceNumStrings $ lines contents
+  -- debug prints
+  -- forM_ fileLines $ \l -> print l
+  return $ sumWholeText fileLines
+
+main :: IO ()
+main = do
+  result <- doIt "./Input/Day1.txt"
+  print result
