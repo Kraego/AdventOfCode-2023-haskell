c693ca9a8e652227d1f2dbe4d796e1cbdc610d9b
kraego <kraego@github.com>
2023-12-09 00:50:29 +0100

    part II Day 5

--- Day5.hs
+++ Day5.hs
@@ -1,17 +1,17 @@
 import Data.List.Split (splitOn, chunksOf)
-import Data.Array ( Ix(inRange) )
+import Data.Array ( Ix(inRange, range) )
 
 -- DestStart SourceStart rangeLength
 type Range = (Int, Int, Int)
 
-dest :: (a, b, c) -> a  
-dest (x, _, _) = x  
-  
-srcRange :: (a, b, c) -> b  
-srcRange (_, y, _) = y  
-  
-rangeLen :: (a, b, c) -> c  
-rangeLen (_, _, z) = z  
+dest :: (a, b, c) -> a
+dest (x, _, _) = x
+
+srcRange :: (a, b, c) -> b
+srcRange (_, y, _) = y
+
+rangeLen :: (a, b, c) -> c
+rangeLen (_, _, z) = z
 
 getSeeds :: [[String]] -> [Int]
 getSeeds xs = map read $ drop 1 $ words $ head $ head xs
@@ -29,8 +29,15 @@ main = do
         ranges = getRanges input
         seeds = getSeeds input
     -- print ranges
-    print $ minimum $ map (getLocation ranges) seeds 
+    print $ minimum $ map (getLocation ranges) seeds
+    print $ minimum $ map (getLocation ranges) $ expandSeeds seeds
 
+-- results in many seed :(
+expandSeeds :: [Int] -> [Int]
+expandSeeds [] = []
+expandSeeds xs =  newRange ++ expandSeeds (drop 2 xs)
+    where
+        newRange  = takeWhile (<=head (tail xs)) $ iterate (+1) $ head xs
 
 getLocation :: [[Range]] -> Int -> Int
 getLocation xs current = foldl (flip adaptCurrent) current xs
@@ -40,3 +47,4 @@ adaptCurrent [] current = current
 adaptCurrent (x:xs) current
     | inRange (srcRange x, srcRange x + rangeLen x) current = dest x + current - srcRange x
     | otherwise = adaptCurrent xs current
+
