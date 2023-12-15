3d07695d9b1f64ffaf872956d60f21dea9038675
kraego <kraego@github.com>
2023-12-15 14:01:19 +0100

    Day 9 - PART II

--- Day09.hs
+++ Day09.hs
@@ -19,27 +19,13 @@ predictHistory [] = []
 predictHistory xs
     | all (==0) xs = [xs]
     | otherwise = xs : predictHistory (predictNextHistory xs)
-    where prediction = predictHistory xs
-
-extrapolate :: [[Int]] -> Int
-extrapolate [] = 0
-extrapolate xs = last appendices
-     where
-         reversed = reverse xs
-         appendices = calculateAppendices (map last reversed) 0
-
-calculateAppendices :: [Int] -> Int -> [Int]
-calculateAppendices (x:xs) prev
-    | null xs = []
-    | x == 0 = 0 : calculateAppendices xs current
-    | otherwise = current : calculateAppendices xs current
-    where current = prev + head xs
 
 main :: IO ()
 main = do
     input <- readFile "./Input/Day09.txt"
     let histories = map toNumbers (lines input)
-        result1 = sum $ map (extrapolate . predictHistory) histories
-    -- print $ map predictHistory histories
-    -- print $ map (extrapolate . predictHistory) histories
+        predictedHistories = map predictHistory histories
+        result1 = sum $ map (foldr ((+) . last) 0) predictedHistories
+        result2 = sum $ map (foldr ((-) . head) 0) predictedHistories
     print result1
+    print result2
